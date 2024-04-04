af19499fe3641d241b6dffca48e016d7e6e6ea26
Carlos Faustino <chfcchfc96@gmail.com>
2024-04-04 12:03:03 -0300

    feat: add swagger

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
+import fastifySwagger from "@fastify/swagger"
+import fastifySwaggerUi from "@fastify/swagger-ui"
 import fastify from "fastify"
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
 } from "fastify-type-provider-zod"
@@ -12,6 +15,23 @@ import { getEventAttendees } from "./routes/get-event-attendees"
 import { registerForEvent } from "./routes/register-for-event"
 
 const app = fastify()
+
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ["application/json"],
+    produces: ["application/json"],
+    info: {
+      title: "pass.in",
+      description:
+        "Especificações da API para o back-end da aplicação pass.in contruída durante o NLW Unite da Rocketseat.",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+})
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
